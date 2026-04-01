b19e4b61469c540b2d4de7507e5bb211abbaa127
t-ski <t.schiepanski@gmx.de>
2026-04-01 15:43:05 +0200

    Update browser snapshot injection environment

--- inject/_inject.js
+++ inject/_inject.js
@@ -1,30 +1,36 @@
 document
     .addEventListener("DOMContentLoaded", async() => {
-        const presentSnapshot = (snapshot, parameters = null) => {
+        const dom = document.body ?? document.documentElement;
+    
+        const presentSnapshot = async (parameters = null, fnCb = null) => {
+            const snapshot = !fnCb
+                ? await D2Snap.d2Snap(
+                    dom,
+                    parameters.k, parameters.l, parameters.m,
+                    { debug: true }
+                )
+                : await fnCb.call(null);
+
             console.log("-".repeat(25));
+            console.log("D2Snap-downsampled DOM snapshot:");
             console.log(snapshot.parameters ?? parameters);
             console.log(snapshot.meta);
             console.log(snapshot.serializedHtml);
         };
 
-        console.log(document.body.innerHTML);
+        console.log("Raw DOM snapshot:\n", dom.outerHTML);
 
-        presentSnapshot(
-            await D2Snap.d2Snap(0.7, 0.7, 0.7, {
-                debug: true
-            }),
-            { k: 0.7, l: 0.7, m: 0.7 }
-        );
-        presentSnapshot(
-            await D2Snap.d2Snap(0.2, 0.4, 0.6, {
-                debug: true
-            }),
-            { k: 0.2, l: 0.4, m: 0.6 }
-        );
-        presentSnapshot(
-            await D2Snap.adaptiveD2Snap(undefined, undefined, {
+        const t0 = performance.now();
+
+        await presentSnapshot({ k: 0.7, l: 0.7, m: 0.7 });
+        await presentSnapshot({ k: 0.2, l: 0.4, m: 0.6 });
+        await presentSnapshot(null, () => D2Snap.adaptiveD2Snap(
+            dom,
+            undefined, undefined, {
                 debug: true,
                 assignUniqueIDs: true
             })
         );
+
+        console.debug(`... ${performance.now() - t0}ms`);
     });
